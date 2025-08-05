3e2ee0e4edfaf59977951c54121355c934d9a064
Jason Olejarz <104961898+jolejarz@users.noreply.github.com>
2025-08-04 20:52:58 -0400

    Update seven-segment_display.c

--- seven-segment_display.c
+++ seven-segment_display.c
@@ -343,7 +343,7 @@ int main (int argc, char *argv[])
 	}
 
 	// Train the network.
-	annlTrain (sequence, layer_input, loss_diff, 128, NULL, step, status);
+	annlTrain (sequence, layer_input, annlCalculateLossSquaredError, loss_diff, 128, NULL, step, status);
 
 	// Print the outputs.
 	for (i=0; i<NUM; i++)
