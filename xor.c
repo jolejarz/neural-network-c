83e4c2c8e773b8d070002d3d90d47b45c6c61747
Jason Olejarz <104961898+jolejarz@users.noreply.github.com>
2025-08-04 20:54:56 -0400

    Update xor.c

--- xor.c
+++ xor.c
@@ -101,7 +101,7 @@ int main (int argc, char *argv[])
 	}
 
 	// Train the network.
-	annlTrain (sequence, layer_input, loss_diff, 4, NULL, step, status);
+	annlTrain (sequence, layer_input, annlCalculateLossSquaredError, loss_diff, 4, NULL, step, status);
 
 	// Print the outputs.
 	for (i=0; i<NUM; i++)
